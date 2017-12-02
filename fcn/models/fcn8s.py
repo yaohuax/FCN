3299e187ba2ccf9057e1e96367774a1140274516
bartyang9 <yikuangy@gmail.com>
2017-12-01 19:59:01 -0500

    change a little bit

--- fcn/models/fcn8s.py
+++ fcn/models/fcn8s.py
@@ -4,25 +4,25 @@ import numpy as np
 import torch
 import torch.nn as nn
 
-import .fcn32 import get_upsampling_weight
+from .fcn32 import get_upsampling_weight
 
-def FCN8s(nn.Module):
 
-	def __init__(self):
-		super(FCN8s, self).__init__:
-        #conv1
-		self.conv1_1 = nn.Conv2d(3, 64, 3, padding = 100)
-		self.relu1_1 = nn.ReLU(inplace = True)
-		self.conv1_2 = nn.Conv2d(64, 64, padding = 1)
-		self.relu1_2 = nn.ReLU(inplace = True)
-		self.pool1 = nn.MaxPool2d(2, stride=2, ceil_mode=True)
+class FCN8s(nn.Module):
+    def __init__(self, n_class=21):
+        super(FCN8s, self).__init__()
+        # conv1
+        self.conv1_1 = nn.Conv2d(3, 64, 3, padding=100)
+        self.relu1_1 = nn.ReLU(inplace=True)
+        self.conv1_2 = nn.Conv2d(64, 64, 3, padding=1)
+        self.relu1_2 = nn.ReLU(inplace=True)
+        self.pool1 = nn.MaxPool2d(2, stride=2, ceil_mode=True)
 
-		# conv2
+        # conv2
         self.conv2_1 = nn.Conv2d(64, 128, 3, padding=1)
         self.relu2_1 = nn.ReLU(inplace=True)
         self.conv2_2 = nn.Conv2d(128, 128, 3, padding=1)
         self.relu2_2 = nn.ReLU(inplace=True)
-        self.pool2 = nn.MaxPool2d(2, stride=2, ceil_mode=True)  
+        self.pool2 = nn.MaxPool2d(2, stride=2, ceil_mode=True)
 
         # conv3
         self.conv3_1 = nn.Conv2d(128, 256, 3, padding=1)
@@ -31,7 +31,7 @@ def FCN8s(nn.Module):
         self.relu3_2 = nn.ReLU(inplace=True)
         self.conv3_3 = nn.Conv2d(256, 256, 3, padding=1)
         self.relu3_3 = nn.ReLU(inplace=True)
-        self.pool3 = nn.MaxPool2d(2, stride=2, ceil_mode=True) 
+        self.pool3 = nn.MaxPool2d(2, stride=2, ceil_mode=True)
 
         # conv4
         self.conv4_1 = nn.Conv2d(256, 512, 3, padding=1)
@@ -40,7 +40,7 @@ def FCN8s(nn.Module):
         self.relu4_2 = nn.ReLU(inplace=True)
         self.conv4_3 = nn.Conv2d(512, 512, 3, padding=1)
         self.relu4_3 = nn.ReLU(inplace=True)
-        self.pool4 = nn.MaxPool2d(2, stride=2, ceil_mode=True) 
+        self.pool4 = nn.MaxPool2d(2, stride=2, ceil_mode=True)
 
         # conv5
         self.conv5_1 = nn.Conv2d(512, 512, 3, padding=1)
@@ -49,7 +49,7 @@ def FCN8s(nn.Module):
         self.relu5_2 = nn.ReLU(inplace=True)
         self.conv5_3 = nn.Conv2d(512, 512, 3, padding=1)
         self.relu5_3 = nn.ReLU(inplace=True)
-        self.pool5 = nn.MaxPool2d(2, stride=2, ceil_mode=True)  
+        self.pool5 = nn.MaxPool2d(2, stride=2, ceil_mode=True)
 
         # fc6
         self.fc6 = nn.Conv2d(512, 4096, 7)
@@ -133,8 +133,8 @@ def FCN8s(nn.Module):
 
         h = self.score_pool3(pool3)
         h = h[:, :,
-              9:9 + upscore_pool4.size()[2],
-              9:9 + upscore_pool4.size()[3]]
+            9:9 + upscore_pool4.size()[2],
+            9:9 + upscore_pool4.size()[3]]
         score_pool3c = h  # 1/8
 
         h = upscore_pool4 + score_pool3c  # 1/8
@@ -144,7 +144,6 @@ def FCN8s(nn.Module):
 
         return h
 
-   
     def copy_params_from_fcn16s(self, fcn16s):
         for name, l1 in fcn16s.named_children():
             try:
---
+++ train_fcn8s.py
@@ -0,0 +1,70 @@
+import argparse
+import os
+import os.path as osp
+import torch
+from train_fcn32s import get_parameters
+from fcn.datasets.mli import ImageList
+from fcn import Trainer
+import torchvision.transforms as transforms
+from fcn.models import FCN16s
+from fcn.models import FCN32s
+from fcn.models import FCN8s
+
+"could add more"
+configurations = {
+    1: dict(
+        max_iteration=100000,
+        lr=1.0e-12,
+        momentum=0.99,
+        weight_decay=0.0005,
+        interval_validate=4000,
+        fcn32s_pretrained_model='input directory of fcn16 model in here',
+    )
+}
+
+def main():
+    cfg = configurations
+    cuda = torch.cuda.is_available()
+    torch.manual_seed(1337)
+    if cuda:
+        torch.cuda.manual_seed(1337)
+    train_dataloader = torch.utils.data.DataLoader(
+        ImageList(fileList="/home/ji/PycharmProjects/FCN_repo/fcn/datasets/train.txt",
+                  transform=transforms.Compose([
+                      transforms.ToTensor(), ])),
+        shuffle=False,
+        num_workers=8,
+        batch_size=1)
+    model = FCN8s()
+    start_epoch = 0
+    start_iteration = 0
+    fcn16s = FCN16s()
+    fcn16s.load_state_dict(torch.load(cfg[1]['fcn16s_pretrained_model']))
+    model.copy_params_from_fcn32s(fcn16s)
+    if cuda:
+        model = model.cuda()
+    optim = torch.optim.SGD(
+        [
+            {'params': get_parameters(model, bias=False)},
+            {'params': get_parameters(model, bias=True),
+             'lr': cfg[1]['lr'] * 2, 'weight_decay': 0},
+        ],
+        lr=cfg[1]['lr'],
+        momentum=cfg[1]['momentum'],
+        weight_decay=cfg[1]['weight_decay'])
+
+    trainer = Trainer(cuda=False,
+                      model=model,
+                      optimizer=optim,
+                      train_loader=train_dataloader,
+                      val_loader=train_dataloader,
+                      max_iter=cfg[1]['max_iteration'],
+                      size_average=False
+                      )
+    trainer.epoch = start_epoch
+    trainer.iteration = start_iteration
+    trainer.train()
+
+
+if __name__ == '__main__':
+    main()
