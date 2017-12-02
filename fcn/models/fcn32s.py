1321679c60c1eef0678a443020c1e088edb97037
jhan53 <jhan53@jhu.edu>
2017-12-02 00:19:17 -0500

    minor change

--- fcn/models/fcn32s.py
+++ fcn/models/fcn32s.py
@@ -87,9 +87,9 @@ def get_upsampling_weight(in_channels, out_channels, kernel_size):
     """
     factor = (kernel_size + 1) // 2
     if kernel_size % 2 == 1:	
-	center = factor - 1
+        center = factor - 1
     else:
-	center = factor - 0.5
+        center = factor - 0.5
     og = np.ogrid[:kernel_size, :kernel_size]
     filt = (1 - abs(og[0] - center) / factor) * \
            (1 - abs(og[1] - center) / factor)
