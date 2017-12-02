c5f1c05f77c0b85baf03559e2af91a0eeb98e1d4
Yaohua Xu <32582353+yaohuax@users.noreply.github.com>
2017-12-02 17:03:29 -0500

    Update train_fcn16s.py

--- train_fcn16s.py
+++ train_fcn16s.py
@@ -9,24 +9,25 @@ import torchvision.transforms as transforms
 from fcn.models import FCN16s
 from fcn.models import FCN32s
 
-"could add more"
-configurations = {
-    1: dict(
-        max_iteration=100000,
-        lr=1.0e-12,
-        momentum=0.99,
-        weight_decay=0.0005,
-        interval_validate=4000,
-        fcn32s_pretrained_model='input directory of fcn32 model in here',
-    )
-}
+#"could add more"
+#configurations = {
+#    1: dict(
+#        max_iteration=100000,
+#        lr=1.0e-12,
+#        momentum=0.99,
+#        weight_decay=0.0005,
+#        interval_validate=4000,
+#        fcn32s_pretrained_model='input directory of fcn32 model in here',
+#    )
+#}
 
 def main():
-    cfg = configurations
-    cuda = torch.cuda.is_available()
-    torch.manual_seed(1337)
-    if cuda:
-        torch.cuda.manual_seed(1337)
+#    cfg = configurations
+#    cuda = torch.cuda.is_available()
+#    torch.manual_seed(1337)
+#    if cuda:
+#        torch.cuda.manual_seed(1337)
+    file = '/home/yaohuaxu1/FCN/fcn32s_model'
     train_dataloader = torch.utils.data.DataLoader(
         ImageList(fileList="/home/yaohuaxu1/FCN/train.txt",
                   transform=transforms.Compose([
@@ -34,27 +35,28 @@ def main():
         shuffle=False,
         num_workers=8,
         batch_size=1)
-    model = FCN16s()
+    model = FCN16s(n_calss = 2)
     start_epoch = 0
     start_iteration = 0
-    fcn32s = FCN32s()
-    fcn32s.load_state_dict(torch.load(cfg[1]['fcn32s_pretrained_model']))
+    fcn32s = FCN32s(n_class = 2)
+    fcn32s.load_state_dict(torch.load(f = path))
     model.copy_params_from_fcn32s(fcn32s)
-    if cuda:
-        model = model.cuda()
-    optim = torch.optim.SGD(
-        [
-            {'params': get_parameters(model, bias=False)},
-            {'params': get_parameters(model, bias=True),
-             'lr': cfg[1]['lr'] * 2, 'weight_decay': 0},
-        ],
-        lr=cfg[1]['lr'],
-        momentum=cfg[1]['momentum'],
-        weight_decay=cfg[1]['weight_decay'])
+#    if cuda:
+    model = model.cuda()
+    optimizer = torch.optim.Adam(model.parameters(), lr = 0.00001)
+#    optim = torch.optim.SGD(
+#        [
+#            {'params': get_parameters(model, bias=False)},
+#            {'params': get_parameters(model, bias=True),
+#             'lr': cfg[1]['lr'] * 2, 'weight_decay': 0},
+#        ],
+#        lr=cfg[1]['lr'],
+#        momentum=cfg[1]['momentum'],
+#        weight_decay=cfg[1]['weight_decay'])
 
     trainer = Trainer(cuda=False,
                       model=model,
-                      optimizer=optim,
+                      optimizer=optimizer,
                       train_loader=train_dataloader,
                       val_loader=train_dataloader,
                       max_iter=cfg[1]['max_iteration'],
